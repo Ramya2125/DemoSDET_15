3536d237e5b2f3defe1acec3dda10411f3699ca9
Ramya R <Ramya R@DESKTOP-99FK3RL>
2021-06-17 18:25:55 +0530

    user 1 says this is my 2nd commit,i ll push the code

---
+++ SDET_15_Framework_Ramya/src/test/java/com/crm/comcast_Practice/user1.java
@@ -0,0 +1,10 @@
+package com.crm.comcast_Practice;
+
+public class user1 {
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		
+		System.out.println("Hi i am user1");
+	}
+
+}
