c22eb44c25f89e7a58302efe8ae450f3e3818f9f
Ramya R <Ramya R@DESKTOP-99FK3RL>
2021-06-21 09:09:47 +0530

    User1 made one changes

--- SDET_15_Framework_Ramya/src/test/java/com/crm/comcast_Practice/user1.java
+++ SDET_15_Framework_Ramya/src/test/java/com/crm/comcast_Practice/user1.java
@@ -16,6 +16,9 @@ public class user1 {
 		System.out.println("Hi this is my system");	
 		
 		System.out.println("Hello this is user 2 my second branch");
+		
+		
+		System.out.println("Hi i am ammu");
 
 	}
 
